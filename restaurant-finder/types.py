4bcfdb340b15b4694e608c91ff1e3bf7f8d207bd
maskyy <62175286+maskyy@users.noreply.github.com>
2025-02-04 16:05:20 +0300

    Replace N/A -> None

--- restaurant-finder/types.py
+++ restaurant-finder/types.py
@@ -2,9 +2,9 @@ from typing import Literal, TypedDict
 
 
 class SearchCriteria(TypedDict):
-    location: str | Literal["N/A"]
-    cuisine: str | Literal["N/A"]
-    budget: int | Literal["N/A"]
-    rating: float | Literal["N/A"]
-    guests: int | Literal["N/A"]
-    time: str | Literal["N/A"]
+    location: str | None
+    cuisine: str | None
+    budget: int | None
+    rating: float | None
+    guests: int | None
+    time: str | None
